83b3df0d6f6775cc7377733e3ce6135e3032c544
jdcarbeck <jdcarbeck@gmail.com>
2018-03-26 13:19:04 +0100

    commented edition

--- lab 4/SampleAsmApp/App.s
+++ lab 4/SampleAsmApp/App.s
@@ -79,39 +79,33 @@ start
 ;thread0 will begin here the first time
 thread0Start
 	ldr	r1,=IO1DIR
-	ldr	r2,=0x000f0000	;select P1.19--P1.16
-	str	r2,[r1]		;make them outputs
+	ldr	r2,=0x000f0000	
+	str	r2,[r1]		
 	ldr	r1,=IO1SET
-	str	r2,[r1]		;set them to turn the LEDs off
+	str	r2,[r1]		
 	ldr	r2,=IO1CLR
-; r1 points to the SET register
-; r2 points to the CLEAR register
-
-	;Lab one here but polling the count
-	ldr	r5,=0x00100000	; end when the mask reaches this value
 	
-wloop	ldr	r3,=0x00010000	; start with P1.16.
-floop	str	r3,[r2]	   	; clear the bit -> turn on the LED
+	ldr	r5,=0x00100000	
+	
+wloop	ldr	r3,=0x00010000	
+floop	str	r3,[r2]	   	
 
 delay0
-	;delay for about a half second
 	ldr	R8,=0x2000000
 dloop0	subs	R8,R8,#1
 	bne	dloop0
 
-	str	r3,[r1]		;set the bit -> turn off the LED
-	mov	r3,r3,lsl #1	;shift up to next bit. P1.16 -> P1.17 etc.
+	str	r3,[r1]		
+	mov	r3,r3,lsl #1	
 	cmp	r3,r5
 	bne	floop
 	b	wloop
-
 
 ;thread1 will begin here the first time
 thread1Start
 	LDR	R1,=IO0DIR
 	LDR	R2,=0x0001B780
-;   0000 0000 0000 0001 1011 0111 1000 0000
-;   31   27   23   19   15   11   7    3
+
 	STR	R2,[R1]		
 	LDR	R1,=IO0SET
 	STR	R2,[R1]		
@@ -119,53 +113,45 @@ thread1Start
 
 	LDR R3, =4
 	LDR R4, =ttb
-	LDR R5, =0x000FFFF0 ;all leds
+	LDR R5, =0x000FFFF0 
 restart
-	LDR	R6, =15    		;end of table
-	MUL R7, R6, R3		; multiply by 4 for memory sized jumpe
+	LDR	R6, =15    		
+	MUL R7, R6, R3		
 while
-	CMP R6, #0			;for loop counting down from 15
+	CMP R6, #0			
 	BLT restart
-	ADD R8, R4, R7		;start of table plus memory offset
-	LDR R8, [R4, R7]	;load value from table
-	STR R5, [R2]		;turn off all led that are on
-	STR R8, [R1]		;turn on bits specified by value
-;delay for about a half second
+	ADD R8, R4, R7		
+	LDR R8, [R4, R7]	
+	STR R5, [R2]		
+	STR R8, [R1]		
+
 	LDR	R9, =2000000
 dloop	SUBS	R9, R9, #1
 	BNE	dloop
+	
 	SUB R6, R6, #1
 	SUB R7, R7, #4
 	B while
 
 stop	b	stop  		; branch always
 
-
-
 	AREA	InterruptStuff, CODE, READONLY
-irqhan	sub	lr,lr,#4
-	STMFD SP!, {R0 - R1}; LR in this case is the place to return to when we store it 
+
+irqhan	sub	lr,lr,#4		;LR will equal the pc of the last thread
+	STMFD SP!, {R0 - R1}	;Stores the register that will be used onto the stack
 	LDR R0, =threads
 	LDR R1, =threadIndex
 	LDR R1, [R1]
 	LSL R1, R1, #2
-	ADD R0, R0, R1
-	;R0 contains the pointer to the thread stack of current process
-	;ADD R1, R0, #8 ;adjust the stact pointer to the apropriate place in the stack
-	LDR R0, [R0]
-	ADD R1, R0, #8
-	STMEA R1, {R2 - R12, LR}
-	LDMFD SP!, {R2 - R3} ;load the saved registers back
-	STMEA R0, {R2 - R3}
-	;the regs are now saved onto the thread stack
+	ADD R0, R0, R1			;R0 is the pointer to the data structure based index
+	LDR R0, [R0]			
+	ADD R1, R0, #8		
+	STMEA R1, {R2 - R12, LR}	;Store R2-R12, PC into memory 
+	LDMFD SP!, {R2 - R3} 		;Load R0-R1 into R2-R3
+	STMEA R0, {R2 - R3}			;Store R2-R3 into R0-R1 in memory
 
-;change the mode
-	;MRS R0, CPSR
-	;LDR R1, =0x0000000F
-	;ORR R0, R0, R1
-	;MSR CPSR_cxsf, R0
 
-;go to next thread in array if end go to begining
+;go to next thread
 	LDR R0, =threads
 	LDR R1, =threadIndex
 	LDR R2, =threadNum
@@ -173,50 +159,46 @@ irqhan	sub	lr,lr,#4
 	LDR R2, [R2]
 	ADD R3, R3, #1
 	CMP R3, R2
-	BLT endIterate
-	LDR R3, =0
+	BLT endIterate		;Check that the next index is <= (size - 1)
+	LDR R3, =0			;If not wrap to zero
 endIterate
-	STR R3, [R1]
-
-;changes to the next stack pointer 
-	LSL R3, R3, #2
+	STR R3, [R1]		;Store that new index in memory
+	LSL R3, R3, #2		
 	ADD R0, R0, R3
-	LDR R0, [R0]
-	;R0 is now the pointer to the next thread stack
+	LDR R0, [R0]		;R0 is now the pointer to the next thread stack
 	LDR R2, =13
-	LDR R4, [R0, R2, LSL #2] ;load the pc to R4
-	LDMFD R0!, {R2 - R3}
-	STMFD SP!, {R2 - R4} ; stores R0 - R1 and the PC
-	LDMFD R0!, {R2 - R12} ;Load the saved registers of the thread stack
+	LDR R4, [R0, R2, LSL #2] 	;Load the pc from memory to R4
+	LDMFD R0!, {R2 - R3}		;R0-R1
+	STMFD SP!, {R2 - R4} 		;Stores R0 - R1 and the PC
+	LDMFD R0!, {R2 - R12} 		;Load the saved registers of the thread stack
 	
-;this is where we stop the timer from making the interrupt request to the VIC
+
 	ldr	r0,=T0
 	mov	r1,#TimerResetTimer0Interrupt
-	str	r1,[r0,#IR]	   	; remove MR0 interrupt request from timer
+	str	r1,[r0,#IR]	   	
 
-;here we stop the VIC from making the interrupt request to the CPU:
 	ldr	r0,=VIC
 	mov	r1,#0
-	str	r1,[r0,#VectAddr]	; reset VIC
+	str	r1,[r0,#VectAddr]	
 	
-	LDMFD SP!, {R0 - R1, PC}^ ;load the rest of the registers and change the program counter
-
-	AREA	Subroutines, CODE, READONLY
+	LDMFD SP!, {R0 - R1, PC}^ 
 
 	AREA	Stuff, DATA, READWRITE
 	
-	
+	;table for the 7 segment
 ttb DCD 0x00003780,0x00000300,0x00009580,0x00008780,0x0000A300,0x0000A680,0x0000B680,0x00000380,0x0000B780,0x0000A380,0x0000B380,0x0000B600,0x00003480,0x0009700,0x0000B480,0x0000B080	
 
-	
-thread0 DCD 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, thread0Start ;last element is the pc of the thread
+	;data structure for the scheduler
+	;each memory space saves R0-R12, PC for each thread 
+thread0 DCD 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, thread0Start 
 thread1 DCD 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, thread1Start 
 
+	;thread array or queue
+	;queue size
 threadNum DCD 2
-	
+	;index of the queue
 threadIndex DCD 0	
-	
+	;queue = [thread0, thread1]
 threads DCD thread0, thread1
 
-	
 	END
